50abceb80acfbbdb1aa3992438a157b76de7a348
Xavier Babu <xavierbabu.a@gmail.com>
2016-07-31 12:23:06 +0530

    first commit

---
+++ application/views/common/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- <link href='http://fonts.googleapis.com/css?family=Lora:400,700|Open+Sans:400,300,700' rel='stylesheet' type='text/css'> -->
+
+	<link rel="stylesheet" href="<?php echo base_url();?>assert/css/reset.css"> <!-- CSS reset -->
+	<link rel="stylesheet" href="<?php echo base_url();?>assert/css/style.css"> <!-- Resource style -->
+	<script src="<?php echo base_url();?>assert/js/modernizr.js"></script> <!-- Modernizr -->
+  	
+	<title>3D Bold Navigation | CodyHouse</title>
+</head>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,33 @@
+<?php
+require_once 'common/header.php';
+?>
+<body class="nav-md">
+	<div class="container body">
+		<div class="main_container">
+			<?php
+require_once 'common/nav.php';
+?>
+<a href="#cd-nav" class="cd-nav-trigger">
+		Menu<span><!-- used to create the menu icon --></span>
+	</a> <!-- .cd-nav-trigger -->
+	<main>
+		<section class="cd-section index cd-selected">
+			<header>
+				<div class="cd-title">
+					<h2>3D Bold Navigation</h2>
+					
+					<a href=#>
+					Sample
+						<span>
+							<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" style="enable-background:new 0 0 16 16;" xml:space="preserve">
+								<polygon fill="#FFFFFF" points="15,7 4.4,7 8.4,3 7,1.6 0.6,8 0.6,8 0.6,8 7,14.4 8.4,13 4.4,9 15,9 "/>
+							</svg>
+						</span>
+					</a>
+				</div> <!-- .cd-title -->
+			</header>
+
+		</section> <!-- .cd-section -->
+	</main>
+<script src="<?php echo base_url();?>assert/js/jquery-2.1.1.js"></script>
+<script src="<?php echo base_url();?>assert/js/main.js"></script> <!-- Resource jQuery -->
